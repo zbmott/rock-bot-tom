08d1b2f507dae1fde6344e5e061cbd19f62c1125
Zachary Mott-Solom <zach.mott@gmail.com>
2022-11-05 02:21:54 -0500

    Initial commit.

---
+++ rockbottom/main.py
@@ -0,0 +1,58 @@
+import datetime, os
+
+import discord
+
+import messages
+
+
+class Client(discord.Client):
+    def __init__(self, *pos, **kw):
+        super().__init__(*pos, **kw)
+
+        self.awaiting_nickname = {}
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print('-----')
+
+    async def update_nickname(self, member, nickname):
+        if member.id not in self.awaiting_nickname:
+            return
+
+        if datetime.timedelta(minutes=30) < datetime.datetime.now() - self.awaiting_nickname[member.id]['timestamp']:
+            return
+
+        member = self.awaiting_nickname[member.id]['member']
+
+        try:
+            await member.edit(nick=nickname)
+            await member.send(f"Thanks! I updated your nickname to `{nickname}` for you.")
+        except discord.errors.Forbidden:
+            pass
+
+        del self.awaiting_nickname[member.id]
+
+    async def on_message(self, message):
+        if isinstance(message.channel, discord.DMChannel):
+            await self.update_nickname(message.author, message.content)
+
+    async def on_member_update(self, before, after):
+        new_roles = list(map(lambda r: r.name, set(after.roles) - set(before.roles)))
+
+        for role in new_roles:
+            if role == 'Guest':
+                self.awaiting_nickname[before.id] = {
+                    'timestamp': datetime.datetime.now(),
+                    'member': before
+                }
+            try:
+                await before.send(messages.MESSAGES[role].strip())
+            except KeyError:
+                pass
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = Client(intents=intents)
+client.run(os.environ['DISCORD_TOKEN'])
---
+++ rockbottom/messages.py
@@ -0,0 +1,27 @@
+MESSAGES = {
+    'Guest': """
+Welcome to <Rock Bottom>'s Discord server. I'm Tom, I help out around here! To keep things running smoothly, we ask that you **set your Discord nickname to match your in-game character name**. I'm happy to do that for you if you **tell me what your name is**.
+    """,
+    'Guild Member': """
+Welcome to the guild! We're excited for you to be here! Let me give you a quick orientation to the way things work.
+
+We have two raid teams: the **weekend** team raids from 6-9pm ST on Sunday and Monday, while the **weekday** team raids from 6-9pm ST on Tuesday and Wednesday. You can find raid signups under the `Raid Signups (10)` and `Raid Signups (25)` channel categories.
+
+The guild bank is open to all guild members -- if you see something you want or need, please make a request in `#🏦｜guild-bank` and an officer will fulfill it for you.
+
+If you're interested in organizing events, ask an officer for the `Event Planner` role so that you can talk to `@raid-helper`.
+
+Finally, please be sure to read `✅｜guild-information` for additional guild rules and policies. If you have any questions, please feel free to reach out to an officer.
+    """,
+    'Event Planner': """
+Thanks for expressing interest in organizing guild events. Now that you're an `Event Planner`, you can interact with `@raid-helper` and manage channels under the `Raid Signups (10)` and `Raid Signups (25)` categories.
+
+Use `/create` to start the event creation dialogue. Sometimes it's easier to `/copy` an existing event and then `/edit` the date and time. You can find complete documentation at `https://raid-helper.com/commands/`. Feel free to reach out to an `Officer` or `Adruit` if you have questions.
+    """,
+    'Raid Officer': """
+Congratulations on your promotion to Raid Officer! Raid officers have access to all the tabs of the guild bank. Please use this privilege responsibly in support of your raid team.
+    """,
+    'Officer': """
+Welcome to the inner circle. Now that you're an `Officer`, you have access to the `#👮｜officer-chat` and `officer-lounge` channels in Discord. You also have full access to the guild bank and officer chat (`/o` or `/officer`) in-game.
+    """
+}

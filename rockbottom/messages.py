a5f060881b73e46efcbbfefb6a7638addfba3945
Zachary Mott <zbmott@users.noreply.github.com>
2024-01-04 00:00:16 -0600

    Update messages.py for <CLAM LORDS>

--- rockbottom/messages.py
+++ rockbottom/messages.py
@@ -1,30 +1,24 @@
 MESSAGES = {
     'Guest': """
-Welcome to <Rock Bottom>'s Discord server. I'm Tom, I help out around here! To keep things running smoothly, we ask that you **set your Discord nickname to match your in-game character name**. I'm happy to do that for you if you **tell me what your name is**.
-    """,
-    'Filthy Degenerate': """
-
+Welcome to <CLAM LORDS>! I'm Tom, I help out around here! To keep things running smoothly, we ask that you **set your Discord nickname to match your in-game character name**. I'm happy to do that for you if you **tell me what your name is**.
     """,
     'Guild Member': """
 Welcome to the guild! We're excited for you to be here! Let me give you a quick orientation to the way things work.
 
-We have two raid teams: the **weekend** team raids from 6-9pm ST on Sunday and Monday, while the **weekday** team raids from 6-9pm ST on Tuesday and Wednesday. You can find raid signups under the `Raid Signups (10)` and `Raid Signups (25)` channel categories.
+Raid signups are organized by weekday and by team name in the `Raid Zone` channel category. We have many raid teams. If you can't find one that works for you, we encourage you to organize one yourself.
 
-The guild bank is open to all guild members -- if you see something you want or need, please make a request in `#🏦｜guild-bank` and an officer will fulfill it for you.
+If you're interested in organizing events, ask a member of the `Clam Police` for the `Event Planner` role so that you can talk to `@raid-helper`.
 
-If you're interested in organizing events, ask an officer for the `Event Planner` role so that you can talk to `@raid-helper`.
-
-Finally, please be sure to read `✅｜guild-information` for additional guild rules and policies. If you have any questions, please feel free to reach out to an officer.
+Finally, please be sure to read `✅｜guild-information` for additional guild rules and policies. If you have any questions, please feel free to reach out to a clam policeman.
     """,
     'Event Planner': """
-Thanks for expressing interest in organizing guild events. Now that you're an `Event Planner`, you can interact with `@raid-helper` and manage channels under the `Raid Signups (10)` and `Raid Signups (25)` categories.
+Thanks for stepping up to organize guild events! Now that you're an `Event Planner`, you can interact with `@raid-helper` and manage channels under the `Raid Zone` channel category.
+ 
+Use `/create` to start the event creation dialogue. Sometimes it's easier to `/copy` an existing event and then `/edit` the date and time. You can find complete documentation at `https://raid-helper.com/commands/`. Feel free to reach out to anyone with the `Clam Police` rank or `Metaslave` if you have questions.
 
-Use `/create` to start the event creation dialogue. Sometimes it's easier to `/copy` an existing event and then `/edit` the date and time. You can find complete documentation at `https://raid-helper.com/commands/`. Feel free to reach out to an `Officer` or `Adruit` if you have questions.
+`Event Planners` can coordinate with each other in `#🪖｜organizer-chat`. Please also check the pins in that channel to get an idea of where to put your events.
     """,
-    'Raid Officer': """
-Congratulations on your promotion to Raid Officer! Raid officers have access to all the tabs of the guild bank. Please use this privilege responsibly in support of your raid team.
-    """,
-    'Officer': """
-Welcome to the inner circle. Now that you're an `Officer`, you have access to the `#👮｜officer-chat` and `officer-lounge` channels in Discord. You also have full access to the guild bank and officer chat (`/o` or `/officer`) in-game.
+    'Clam Police': """
+Welcome to the inner circle. Now that you're a member of the `Clam Police`, you have access to the `👮｜police-chat` and `🏦｜police hq` channels in Discord. You also have access to officer chat (`/o` or `/officer`) in-game.
     """
 }
